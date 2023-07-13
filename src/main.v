97a43fd5444b01ea25e00396320af230f3e3814e
nmke-de <github@nmke.de>
2023-07-13 06:27:29 +0200

    Implemented simple echo server on nex://

--- src/main.v
+++ src/main.v
@@ -1,5 +1,15 @@
 module main
 
+import net
+
 fn main() {
-	println('Hello World!')
+	mut listener := net.listen_tcp(net.AddrFamily.ip, ':1900')!
+
+	// Loop
+	for {
+		mut connection := listener.accept()!
+		path := connection.read_line()
+		connection.write_string('${path}\r\n')!
+		connection.close()!
+	}
 }
