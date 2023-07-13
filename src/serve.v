03ec3d4b1998ecf033f5d635c979eb371c8a52a3
nmke-de <github@nmke.de>
2023-07-13 11:41:44 +0200

    Added checks to not go .. from served root.

--- src/serve.v
+++ src/serve.v
@@ -4,11 +4,11 @@ import net
 import os
 
 fn serve(mut connection net.TcpConn) {
-	path := connection.read_line().all_before('\r\n').all_after('/')
-	result := if os.is_dir(path) {
-		ls('${os.getwd()}/${path}')
-	} else if path == '' {
+	path := connection.read_line().all_before('\r\n').replace_each(['../', '/']).all_after('/')
+	result := if path == '..' || path.ends_with('/..') || path == '' {
 		ls(os.getwd())
+	} else if os.is_dir(path) {
+		ls('${os.getwd()}/${path}')
 	} else {
 		cat(path)
 	}
