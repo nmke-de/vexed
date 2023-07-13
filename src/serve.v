22759b27f835d5ee5601cda02978066f89b8b3e9
nmke-de <github@nmke.de>
2023-07-13 07:57:24 +0200

    Using ls() and cat() now

--- src/serve.v
+++ src/serve.v
@@ -1,9 +1,17 @@
 module main
 
 import net
+import os
 
 fn serve(mut connection net.TcpConn) {
-	path := connection.read_line()
-	connection.write_string('${path}\r\n') or {}
+	path := connection.read_line().all_before('\r\n')
+	result := if os.is_dir(path) {
+		ls('${os.getwd()}/${path}')
+	} else if path == '' {
+		ls(os.getwd())
+	} else {
+		cat(path)
+	}
+	connection.write_string(result) or {}
 	connection.close() or {}
 }
