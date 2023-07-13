998ced2fa0b3a5c63f16041991c81e639c5f479a
nmke-de <github@nmke.de>
2023-07-13 06:38:29 +0200

    Format

--- src/serve.v
+++ src/serve.v
@@ -2,7 +2,7 @@ module main
 
 import net
 
-fn serve(mut connection &net.TcpConn) {
+fn serve(mut connection net.TcpConn) {
 	path := connection.read_line()
 	connection.write_string('${path}\r\n') or {}
 	connection.close() or {}
