7b8447ab82efde82902b138a33b50d55ca77caac
nmke-de <github@nmke.de>
2023-07-13 23:22:04 +0200

    Index can be CGI served now

--- src/cat.v
+++ src/cat.v
@@ -4,6 +4,9 @@ import io
 import os
 
 fn cat(filename string) string {
+	if os.is_executable(filename) {
+		return cgi(filename)
+	}
 	mut f := os.open(filename) or { return '' }
 	defer {
 		f.close()
--- src/serve.v
+++ src/serve.v
@@ -6,12 +6,22 @@ import os
 fn serve(mut connection net.TcpConn) {
 	request := connection.read_line().all_before('\r\n')
 	path := request.replace_each(['../', '/']).all_after('/').all_before('?')
+
+	// Environment variables for CGI
+	os.setenv('QUERY_STRING', query_string(request), true)
+	os.setenv('REMOTE_ADDR', (connection.addr() or {
+		net.Addr{
+			f: 0
+		}
+	}).str(), true)
+	os.setenv('SCRIPT_NAME', '${os.hostname() or { 'localhost' }}/${path}', true)
+
 	result := if path == '..' || path.ends_with('/..') || path == '' {
 		ls(os.getwd())
 	} else if os.is_dir(path) {
 		ls(path)
 	} else if os.is_executable(path) {
-		cgi(connection, request, path)
+		cgi(path)
 	} else {
 		cat(path)
 	}
